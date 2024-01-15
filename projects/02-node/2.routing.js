30ae1fa2322ff2b1e180fb789308697c4c567895
Diego Ludeña <diegolude.martin@gmail.com>
2024-01-15 19:57:44 +0100

    API con Express con Middleware

--- projects/02-node/2.routing.js
+++ projects/02-node/2.routing.js
@@ -1,7 +1,48 @@
 const http = require('node:http')
 
-const processRequest = (req, res) => {
+const dittoJSON = require('./pokemon/ditto.json')
 
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404 // Not Found
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          res.end('<h1>404</h1>')
+      }
+      break
+    case 'POST':
+      switch (url) {
+        case '/pokemon':{
+          let body = ''
+
+          // escuchar el evento data
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // llamar a base de datos para guardar la info
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+            res.end(JSON.stringify(data))
+          })
+
+          break
+        }
+
+        default:
+          res.statusCode = 404 // Not Found
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          res.end('<h1>404</h1>')
+      }
+  }
 }
 
 const server = http.createServer(processRequest)
